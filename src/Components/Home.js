2cd6fcc9e41c86d84b42db72da06e8b1ad7d897a
fshrabon151 <fshrabon151@gmail.com>
2021-11-21 18:58:02 +0600

    Home (profile) component created

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,220 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Button, Grid } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import { CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Spinner from './layouts/Spinner';
+import MetaData from './layouts/MetaData';
 
 const Home = () => {
-  return <div></div>;
+  const { user, loading } = useSelector((state) => state.auth);
+  return (
+    <>
+      <CssBaseline />
+
+      {loading && user === null ? (
+        <Spinner />
+      ) : (
+        <>
+          <Container maxWidth="lg">
+            <MetaData title="Home" />
+            <Box sx={{ my: 10 }}>
+              <Typography variant="h4" component="h2" sx={{ mb: 5 }}>
+                My Profile
+              </Typography>
+
+              <Grid
+                container
+                spacing={3}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  bgColor: '#e3f2fd',
+                }}
+              >
+                <Grid item xs={12} md={6}>
+                  <Card
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      flexDirection: 'column',
+                      bgcolor: '#eceff1',
+                      height: '100%',
+                      boxShadow: 0,
+                      py: 10,
+                    }}
+                  >
+                    <Avatar
+                      src={user && user.avatar.url}
+                      sx={{ width: 256, height: 256 }}
+                    />
+                  </Card>
+                </Grid>
+                <Grid item xs={12} md={6} sx={{ paddingRight: 3 }}>
+                  <Card
+                    sx={{
+                      padding: 2,
+                      pb: 5,
+                      bgcolor: '#eceff1',
+                      boxShadow: 0,
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{
+                        textAlign: 'center',
+                        my: 3,
+                        textTransform: 'capitalize',
+                      }}
+                    >
+                      MetaData
+                    </Typography>
+                    <Grid
+                      container
+                      spacing={2}
+                      sx={{ justifyContent: 'center' }}
+                    >
+                      <Grid item md={6}>
+                        <Card>
+                          <CardActionArea>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                              >
+                                First Name
+                              </Typography>
+                              <Typography variant="body2" color="text.primary">
+                                {user && user.firstName}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+                      <Grid item md={6}>
+                        <Card>
+                          <CardActionArea>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                              >
+                                Last Name
+                              </Typography>
+                              <Typography variant="body2" color="text.primary">
+                                {user && user.lastName}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+                      <Grid item md={6}>
+                        <Card>
+                          <CardActionArea>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                              >
+                                Email
+                              </Typography>
+                              <Typography variant="body1" color="text.primary">
+                                {user && user.email}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+                      <Grid item md={6}>
+                        <Card>
+                          <CardActionArea>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                              >
+                                Phone
+                              </Typography>
+                              <Typography variant="body2" color="text.primary">
+                                {user && user.phone}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          fullWidth
+                          size="small"
+                          sx={{ mt: 3, mb: 2 }}
+                        >
+                          <Link
+                            to="/password-update"
+                            style={{
+                              textDecoration: 'none',
+                              color: 'white',
+                              display: 'inline-block',
+                              width: '100%',
+                            }}
+                          >
+                            Change Password
+                          </Link>
+                        </Button>
+                        <Box sx={{ width: 25 }}></Box>
+
+                        <Button
+                          variant="contained"
+                          fullWidth
+                          sx={{ mt: 3, mb: 2 }}
+                        >
+                          <Link
+                            to="/profile-update"
+                            style={{
+                              textDecoration: 'none',
+                              color: 'white',
+                              display: 'inline-block',
+                              width: '100%',
+                            }}
+                          >
+                            Update Profile
+                          </Link>
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Card>
+                </Grid>
+              </Grid>
+            </Box>
+          </Container>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;

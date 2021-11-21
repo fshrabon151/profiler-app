f3f6b3247038ca74a20cdc7f17705da2b79e6362
fshrabon151 <fshrabon151@gmail.com>
2021-11-21 18:36:42 +0600

    Login component created, initialize login function in redux

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,7 +1,118 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Login = () => {
-  return <div></div>;
-};
+import { useDispatch, useSelector } from 'react-redux';
+import { useAlert } from 'react-alert';
+import { useNavigate } from 'react-router-dom';
+import { clearErrors, login } from '../../redux/actions/auth';
+import MetaData from '../layouts/MetaData';
 
-export default Login;
+const theme = createTheme();
+
+export default function Login() {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const { isAuthenticated, loading, error } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate(`/`);
+    }
+  }, [isAuthenticated, navigate]);
+  if (error) {
+    alert.error(error);
+    dispatch(clearErrors());
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <MetaData title="Login" />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={submitHandler}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={loading ? true : false}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -39,6 +39,28 @@ export const register = (userData) => async (dispatch) => {
   }
 };
 
+// Login
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+    };
+    const { data } = await axios.post(
+      `/api/v1/login`,
+      {
+        email,
+        password,
+      },
+      config
+    );
+
+    dispatch({ type: LOGIN_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
 // Load user
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {

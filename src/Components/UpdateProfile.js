14f417be79996e9fb0ed21e048a554d9f4be8c85
fshrabon151 <fshrabon151@gmail.com>
2021-11-21 19:14:41 +0600

    Profile Update functionality done

--- src/Components/UpdateProfile.js
+++ src/Components/UpdateProfile.js
@@ -1,7 +1,213 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearErrors, loadUser, updateProfile } from '../redux/actions/auth';
+import MetaData from './layouts/MetaData';
+
+const theme = createTheme();
 
 const UpdateProfile = () => {
-  return <div></div>;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [avatarPreview, setAvatarPreview] = useState('/broken-image.jpg');
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+  const { error, isUpdated, loading } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setPhone(user.phone);
+      setEmail(user.email);
+      setAvatarPreview(user.avatar.url);
+    }
+
+    if (isUpdated) {
+      alert.success('User updated successfully');
+      dispatch(loadUser());
+      navigate('/');
+    }
+  }, [alert, dispatch, isUpdated, navigate, user]);
+  if (error) {
+    alert.error(error);
+    dispatch(clearErrors());
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set('firstName', firstName);
+    formData.set('lastName', lastName);
+    formData.set('phone', phone);
+    formData.set('email', email);
+    formData.set('avatar', avatar);
+    dispatch(updateProfile(formData));
+  };
+
+  const onChange = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
+        setAvatar(reader.result);
+      }
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <MetaData title="Update Profile" />
+      <Container component="main" maxWidth="md">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Update Profile
+          </Typography>
+          <Box
+            component="form"
+            enctype="multipart/form-data"
+            noValidate
+            onSubmit={submitHandler}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  label="Phone"
+                  name="phone"
+                  autoComplete="phone"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={2}>
+                <Avatar
+                  src={avatarPreview ? avatarPreview : `/broken-image.jpg`}
+                  sx={{ width: 56, height: 56 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={10}>
+                <TextField
+                  accept="image/*"
+                  type="file"
+                  required
+                  name="photo"
+                  id="photo"
+                  fullWidth
+                  onChange={onChange}
+                />
+              </Grid>
+            </Grid>
+
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                variant="contained"
+                color="secondary"
+                fullWidth
+                sx={{ mt: 3, mb: 2 }}
+              >
+                <Link
+                  to="/"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    display: 'inline-block',
+                    width: '100%',
+                  }}
+                >
+                  Go back
+                </Link>
+              </Button>
+              <Box sx={{ width: 25 }}></Box>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={loading ? true : false}
+              >
+                Update
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default UpdateProfile;
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -77,6 +77,25 @@ export const loadUser = () => async (dispatch) => {
   }
 };
 
+// Update user profile
+export const updateProfile = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PROFILE_REQUEST });
+    const config = {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    };
+    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+
+    dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
+    dispatch({ type: UPDATE_PROFILE_RESET });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 // Update password
 export const updatePassword = (passwords) => async (dispatch) => {
   try {
